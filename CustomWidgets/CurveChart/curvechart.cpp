13d75bd07482646680252519f6e12a5158a88988
zhouyuming <1006129869@qq.com>
2018-10-21 11:40:16 +0800

    Add files via upload

---
+++ CustomWidgets/CurveChart/curvechart.cpp
@@ -0,0 +1,367 @@
+﻿#include "curvechart.h"
+#include <QPainter>
+#include <QtMath>
+#include <QDebug>
+
+CurveChart::CurveChart(QWidget *parent)
+    : QWidget(parent)
+{
+    m_bgStartColor = QColor(79, 79, 79);
+    m_bgEndColor = QColor(51, 51, 51);
+    m_textColor = QColor(Qt::white);
+    m_pointColor = QColor(38, 114, 179);
+    m_min = 0;
+    m_max = 100;
+    m_stepH = 10;
+    m_stepV = 10;
+    m_chartLeftMargin = 50;
+    m_chartTopMargin = 50;
+    m_chartRightMargin = 10;
+    m_chartBottomMargin = 10;
+    m_pointRadious = 2;
+    m_showLine = true;
+    m_showPoint = true;
+    m_showPointBg = true;
+}
+
+CurveChart::~CurveChart()
+{
+
+}
+
+void CurveChart::setBackgroundStartColor(QColor color)
+{
+    if(color != m_bgStartColor)
+    {
+        m_bgStartColor = color;
+        update();
+    }
+}
+
+void CurveChart::setBackgroundEndColor(QColor color)
+{
+    if(color != m_bgEndColor)
+    {
+        m_bgEndColor = color;
+        update();
+    }
+}
+
+void CurveChart::setTextColor(QColor color)
+{
+    if(color != m_textColor)
+    {
+        m_textColor = color;
+        update();
+    }
+}
+
+void CurveChart::setPointColor(QColor color)
+{
+    if(color != m_pointColor)
+    {
+        m_pointColor = color;
+        update();
+    }
+}
+
+void CurveChart::setMin(qreal min)
+{
+    setRange(min, m_max);
+}
+
+void CurveChart::setMax(qreal max)
+{
+    setRange(m_min, max);
+}
+
+void CurveChart::setStepHoriginal(qreal val)
+{
+    if(val != m_stepH)
+    {
+        m_stepH = val;
+        update();
+    }
+}
+
+void CurveChart::setStepVertical(qreal val)
+{
+    if(val != m_stepV)
+    {
+        m_stepV = val;
+        update();
+    }
+}
+
+void CurveChart::setChartLeftMargin(qreal margin)
+{
+    if(margin>=0 && margin != m_chartLeftMargin)
+    {
+        m_chartLeftMargin = margin;
+        update();
+    }
+}
+
+void CurveChart::setChartTopMargin(qreal margin)
+{
+    if(margin>=0 && margin != m_chartTopMargin)
+    {
+        m_chartTopMargin = margin;
+        update();
+    }
+}
+
+void CurveChart::setChartRightMargin(qreal margin)
+{
+    if(margin>=0 && margin != m_chartRightMargin)
+    {
+        m_chartRightMargin = margin;
+        update();
+    }
+}
+
+void CurveChart::setChartBottomMargin(qreal margin)
+{
+    if(margin>=0 && margin != m_chartBottomMargin)
+    {
+        m_chartBottomMargin = margin;
+        update();
+    }
+}
+
+QString CurveChart::DataString() const
+{
+    QStringList points;
+    for(int i=0; i<m_points.size(); ++i)
+    {
+        points << QString("%1").arg(m_points[i]);
+    }
+    return points.join("|");
+}
+
+void CurveChart::addDataStr(QString str)
+{
+    clearData();
+    QStringList dataList = str.split("|");
+    foreach(QString valStr, dataList)
+    {
+        bool ok;
+        double val = valStr.toDouble(&ok);
+        if(ok)
+        {
+            addData(val);
+        }
+    }
+}
+
+void CurveChart::addData(qreal val)
+{
+    int maxNum = qFloor((width()-m_chartLeftMargin-m_chartRightMargin)/m_stepH);
+    if(maxNum < 0) { return; }
+    if(m_points.size()>maxNum)
+    {
+        m_points.pop_front();
+    }
+    m_points.push_back(val);
+    update();
+}
+
+void CurveChart::setTitle(QString str)
+{
+    if(str != m_title)
+    {
+        m_title = str;
+        update();
+    }
+}
+
+void CurveChart::setShowLine(bool show)
+{
+    if(show != m_showLine)
+    {
+        m_showLine = show;
+        update();
+    }
+}
+
+void CurveChart::setShowPoint(bool show)
+{
+    if(show != m_showPoint)
+    {
+        m_showPoint = show;
+        update();
+    }
+}
+
+void CurveChart::setShowPointBackground(bool show)
+{
+    if(show != m_showPointBg)
+    {
+        m_showPointBg = show;
+        update();
+    }
+}
+
+void CurveChart::clearData()
+{
+    if(m_points.size()>0)
+    {
+        m_points.clear();
+        update();
+    }
+}
+
+void CurveChart::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    drawBackground(&painter);
+    drawTitle(&painter);
+    drawBorder(&painter);
+    drawText(&painter);
+    drawPoints(&painter);
+}
+
+void CurveChart::drawBackground(QPainter *p)
+{
+    p->save();
+    QLinearGradient linearGradient(QPointF(0,0), QPointF(0,height()));
+    linearGradient.setColorAt(0.0, m_bgStartColor);
+    linearGradient.setColorAt(1.0, m_bgEndColor);
+    p->setPen(Qt::NoPen);
+    p->setBrush(linearGradient);
+    p->drawRect(rect());
+    p->restore();
+}
+
+void CurveChart::drawTitle(QPainter *p)
+{
+    p->save();
+    QPointF topLeft(0, 0);
+    QPointF bottomRight(width(), m_chartTopMargin);
+    QRectF titleRect(topLeft, bottomRight);
+    p->setPen(m_textColor);
+    p->drawText(titleRect, Qt::AlignCenter, m_title);
+    p->restore();
+}
+
+void CurveChart::drawBorder(QPainter *p)
+{
+    p->save();
+    //画边框
+    QPointF topLeft(m_chartLeftMargin, m_chartTopMargin);
+    QPointF bottomRight(width()-m_chartRightMargin, height()-m_chartBottomMargin);
+    QRectF borderRect(topLeft, bottomRight);
+    p->setPen(m_textColor);
+    p->drawRect(borderRect);
+    //画线条
+    if(m_showLine)
+    {
+        QPen pen = p->pen();
+        pen.setStyle(Qt::DotLine);
+        p->setPen(pen);
+        qreal count = (m_max-m_min)/m_stepV;
+        qreal stepY = borderRect.height()/count;
+        qreal dy = m_chartTopMargin+stepY;
+        QPointF p1,p2;
+        while(dy<borderRect.bottom())
+        {
+            p1 = QPointF(borderRect.left(), dy);
+            p2 = QPointF(borderRect.right(), dy);
+            p->drawLine(p1, p2);
+            dy += stepY;
+        }
+    }
+    p->restore();
+}
+
+void CurveChart::drawText(QPainter *p)
+{
+    p->save();
+    qreal textMargin = 5;
+    QFontMetrics fm = fontMetrics();
+    int textHeight = fm.height();
+    QPointF topLeft(m_chartLeftMargin, m_chartTopMargin);
+    QPointF bottomRight(width()-m_chartRightMargin, height()-m_chartBottomMargin);
+    QRectF borderRect(topLeft, bottomRight);
+    qreal count = (m_max-m_min)/m_stepV;
+    qreal stepY = borderRect.height()/count;
+    qreal dy = m_chartTopMargin;
+    QPointF pos;
+    qreal val = m_max;
+    p->setPen(m_textColor);
+    while(val >= m_min)
+    {
+        QString text = QString("%1").arg(val);
+        int textWidth = fm.width(text);
+        pos = QPointF(m_chartLeftMargin-textMargin-textWidth, dy+textHeight/2.0);
+        p->drawText(pos, text);
+        dy += stepY;
+        val -= m_stepV;
+    }
+    p->restore();
+}
+
+void CurveChart::drawPoints(QPainter *p)
+{
+    if(m_points.isEmpty())
+        return;
+    p->save();
+    QVector<QPointF> posVec;
+
+    qreal dx = width()-m_chartRightMargin;
+    qreal dy = height()-m_chartBottomMargin;
+    qreal percent = (height()-m_chartTopMargin-m_chartBottomMargin)/(m_max-m_min);
+    posVec.append(QPointF(dx, dy));//结束点
+    QVector<qreal>::reverse_iterator it;
+    for(it = m_points.rbegin(); it!= m_points.rend(); ++it)
+    {
+        if(dx<m_chartLeftMargin)
+        {
+            break;
+        }
+        posVec.append(QPointF(dx, dy-(*it-m_min)*percent));
+        dx -= m_stepH;
+    }
+    posVec.append(QPointF(dx<m_chartLeftMargin?m_chartLeftMargin:dx, dy));
+    if(!m_showPointBg)
+    {//不显示背景的话去除起始点和结束点
+        posVec.pop_front();
+        posVec.pop_back();
+    }
+    //画圆点
+    if(m_showPoint)
+    {
+        int radious = m_stepH/2;
+        p->save();
+        p->setPen(Qt::NoPen);
+        p->setBrush(m_pointColor);
+        for(int i=0; i<posVec.size(); ++i)
+        {
+            p->drawEllipse(posVec[i], radious, radious);
+        }
+        p->restore();
+    }
+    //画线
+    p->setPen(m_pointColor);
+    if(m_showPointBg)
+    {
+        p->setBrush(m_pointColor);
+        p->setOpacity(0.5);
+        p->drawConvexPolygon(QPolygonF(posVec));
+    }
+    else
+    {
+        p->drawPolyline(QPolygonF(posVec));
+    }
+    p->restore();
+}
+
+void CurveChart::setRange(qreal min, qreal max)
+{
+    if(min>max)
+        qSwap(min, max);
+    m_min = min;
+    m_max = max;
+    update();
+}
---
+++ CustomWidgets/CurveChart/curvechart.h
@@ -0,0 +1,109 @@
+﻿#ifndef CURVECHART_H
+#define CURVECHART_H
+
+#include <QWidget>
+
+#ifdef MSC
+#if (QT_VERSION < QT_VERSION_CHECK(5,7,0))
+#include <QtDesigner/QDesignerExportWidget>
+#else
+#include <QtUiPlugin/QDesignerExportWidget>
+#endif
+
+class QDESIGNER_WIDGET_EXPORT CurveChart: public QWidget
+#else
+class CurveChart : public QWidget
+#endif
+
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QColor BackgroundStartColor READ BackgroundStartColor WRITE setBackgroundStartColor)
+    Q_PROPERTY(QColor BackgroundEndColor READ BackgroundEndColor WRITE setBackgroundEndColor)
+    Q_PROPERTY(QColor TextColor READ TextColor WRITE setTextColor)
+    Q_PROPERTY(QColor PointColor READ PointColor WRITE setPointColor)
+    Q_PROPERTY(qreal Min READ Min WRITE setMax)
+    Q_PROPERTY(qreal Max READ Max WRITE setMax)
+    Q_PROPERTY(qreal StepHoriginal READ StepHoriginal WRITE setStepHoriginal)
+    Q_PROPERTY(qreal StepVertical READ StepVertical WRITE setStepVertical)
+    Q_PROPERTY(qreal ChartLeftMargin READ ChartLeftMargin WRITE setChartLeftMargin)
+    Q_PROPERTY(qreal ChartTopMargin READ ChartTopMargin WRITE setChartTopMargin)
+    Q_PROPERTY(qreal ChartRightMargin READ ChartRightMargin WRITE setChartRightMargin)
+    Q_PROPERTY(qreal ChartBottomMargin READ ChartBottomMargin WRITE setChartBottomMargin)
+    Q_PROPERTY(QString DataString READ DataString WRITE addDataStr)
+
+public:
+    CurveChart(QWidget *parent = 0);
+    ~CurveChart();
+
+    QColor BackgroundStartColor() const { return m_bgStartColor; }
+    void setBackgroundStartColor(QColor color);
+    QColor BackgroundEndColor() const { return m_bgEndColor; }
+    void setBackgroundEndColor(QColor color);
+    QColor TextColor() const { return m_textColor; }
+    void setTextColor(QColor color);
+    QColor PointColor() const { return m_pointColor; }
+    void setPointColor(QColor color);
+    qreal Min() const { return m_min; }
+    void setMin(qreal min);
+    qreal Max() const { return m_max; }
+    void setMax(qreal max);
+    qreal StepHoriginal() const { return m_stepH; }
+    void setStepHoriginal(qreal val);
+    qreal StepVertical() const { return m_stepV; }
+    void setStepVertical(qreal val);
+    qreal ChartLeftMargin() const { return m_chartLeftMargin; }
+    void setChartLeftMargin(qreal margin);
+    qreal ChartTopMargin() const { return m_chartTopMargin; }
+    void setChartTopMargin(qreal margin);
+    qreal ChartRightMargin() const { return m_chartRightMargin; }
+    void setChartRightMargin(qreal margin);
+    qreal ChartBottomMargin() const { return m_chartBottomMargin; }
+    void setChartBottomMargin(qreal margin);
+    QString DataString() const;
+    void addDataStr(QString str);
+    void addData(qreal val);
+
+public slots:
+    void setTitle(QString str);
+    void setShowLine(bool show);
+    void setShowPoint(bool show);
+    void setShowPointBackground(bool show);
+    void clearData();
+
+protected:
+    void paintEvent(QPaintEvent *);
+    void drawBackground(QPainter* p);
+    void drawTitle(QPainter* p);
+    void drawBorder(QPainter* p);
+    void drawText(QPainter* p);
+    void drawPoints(QPainter* p);
+
+    QSize minimumSizeHint() const { return QSize(400, 250); }
+
+private:
+    void setRange(qreal min, qreal max);
+
+private:
+    QColor m_bgStartColor;//背景颜色
+    QColor m_bgEndColor;
+    QColor m_textColor;//标题/文本/边框颜色
+    QColor m_pointColor;//统计点颜色
+    QString m_title;//标题文本
+    qreal m_min;//最小值
+    qreal m_max;//最大值
+    qreal m_stepH;//水平间隔
+    qreal m_stepV;//垂直间隔
+    qreal m_chartLeftMargin;//图表左边距
+    qreal m_chartTopMargin;
+    qreal m_chartRightMargin;
+    qreal m_chartBottomMargin;
+    int m_pointRadious;//数据点半径
+    bool m_showLine;//显示刻度线
+    bool m_showPoint;//显示数据点
+    bool m_showPointBg;//显示数据点覆盖背景
+
+    QVector<qreal> m_points;
+};
+
+#endif // CURVECHART_H
