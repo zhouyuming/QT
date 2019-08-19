65b6cba242a146323ad5c64f912cca5b1b5cd80e
zhouyuming <zhouyuming@dding.cn>
2019-08-19 15:34:44 +0800

    create MyQtCustomWidgets project

---
+++ MyQtCustomWidgets/q7seg.cpp
@@ -0,0 +1,6 @@
+#include "q7seg.h"
+
+Q7seg::Q7seg(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ MyQtCustomWidgets/q7seg.h
@@ -0,0 +1,17 @@
+#ifndef Q7SEG_H
+#define Q7SEG_H
+
+#include <QWidget>
+
+class Q7seg : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Q7seg(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // Q7SEG_H
